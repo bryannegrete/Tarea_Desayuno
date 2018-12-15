60be48c183dbb2a70c0169837b2defb94d68ca06
bryan_negrete <bryanegrete@gmail.com>
2018-12-15 00:42:25 -0300

    Tarea_4 Se agrega la preparación de un desayuno a través de acciones en R. con respectivos test.

---
+++ Desayuno_Tarea4.R
@@ -0,0 +1,137 @@
+#Tarea_4 Desayuno
+  #Vamos a preparar un desayuno con ciertas funciones para lo cual necesitaremos
+  #diversas materias primas (input) para llevar a cabo las acciones.
+
+#Def: Dada ciertas condiciones de las materias primas (input)
+    # obtendremos los productos para confeccionar el desayuno.
+
+#Las materias primas son:
+  #palta, naranjas, pan, cafe, harina y huevos.
+
+#Las acciones son:
+  #Calentar: String->String
+    #Ejemplo: Calentar("cafe")->"cafe Caliente"
+  #Preparar: String->String
+    #Ejemplo: Preparar("naranjas cortadas")->"jugo de naranjas"
+  #Rellenar: String->String
+    #Ejemplo: Rellenar("panqueques")->"panqueques con manjar"
+  #Moler: String->String
+    #Ejmplo: Moler("palta cortada")->"palta molida"
+  #Cortar: String->String
+    #Ejemplo: Cortar("pan")->"pan cortado"
+  #CalentarSarten: String->String
+    #Ejemplo: CalentarSarten(sarten)->"sarten caliente"
+  #Servir: String->String
+    #Ejmplo:Servir("pan tostado con palta" && 
+                  #"panqueques con manjar" && 
+                  #"jugo de naranjas" &&
+                  #"cafe caliente")->"mmmmm... niami... el desayuno esta maravilloso"
+
+Calentar <- function(x){
+  if(x=="cafe"){
+    return("cafe caliente")
+  }else if(x=="pan cortado"){
+    return("pan tostado")
+  }else{
+    return(paste("No se que hacer con ",x))
+  }
+}
+
+Preparar <- function(x,y){
+  if(x=="naranjas cortadas"){
+    return("jugo de naranjas")
+  }else if((x=="harina" && y=="huevos")||(y=="harina" && x=="huevos")){
+    return("masa de panqueques")
+  }else if((x=="masa de panqueques" && y=="sarten caliente")||(y=="masa de panqueques" && x=="sarten caliente")){
+    return("panqueques")
+  }else if((x=="pan tostado" && y=="palta molida")||(y=="pan tostado" && x=="palta molida")){
+    return("pan tostado con palta")
+  }else{
+    return("faltan elementos")  
+  }
+}
+
+Rellenar <- function(x){
+  if(x=="panqueques"){
+    return("panqueques con manjar")
+  }else{
+    return(paste("No se que hacer con ",x)) 
+  }
+}
+
+Moler <- function(x){
+  if(x=="palta cortada"){
+    return("palta molida")
+  }else{
+    return(paste("No se que hacer con ",x))
+  }
+}
+
+Cortar <- function(x){
+  if(x=="pan"){
+    return("pan cortado")
+  }else if(x=="naranjas"){
+    return("naranjas cortadas")
+  }else if(x=="palta"){
+    return("palta cortada")
+  }else{
+    return(paste("No se que hacer con ",x))
+  }
+}
+
+CalentarSarten <- function(x){
+  if (x=="sarten"){
+    return("sarten caliente")
+  }else{
+    return(paste("No se que hacer con",x)) 
+  }
+}
+
+Servir <- function(x,y,z,j){
+  if((x=="pan tostado con palta" && y=="panqueques con manjar" && z=="jugo de naranjas" && j=="cafe caliente")){
+    return("mmmmm... niami... el desayuno esta maravilloso")
+  }else{
+    return("iiiuuuu... esto no esta listo")
+  }
+}
+
+#Test
+  #Para preparar un desayuno calientito, se realizara¡ el siguiente procedimiento:
+
+#1) 
+   Cortar("naranjas")
+   Cortar("pan")
+   Cortar("palta")
+   
+#2)
+   Preparar("naranjas cortadas")
+   Moler("palta cortada")
+   Preparar("harina","huevos")
+
+#3)
+   CalentarSarten("sarten")
+   Preparar("masa de panqueques","sarten caliente")
+   Rellenar("panqueques")
+   
+#4)
+   Calentar("pan cortado")
+   Preparar("pan tostado","palta molida")
+
+#5)
+   Calentar("cafe")
+
+#6)
+   Servir("pan tostado con palta","panqueques con manjar","jugo de naranjas","cafe caliente")
+   
+#Test2
+#En caso de usar otras materias primas u otros resultados
+#no se podra realizar el desayuno.
+#Tambien, no se puede servir el desayuno si falta algun elemento preparado.
+
+Cortar("manzanas")
+Preparar("pan","huevos")
+Moler("masa de panqueques")
+Rellenar("pan")
+CalentarSarten("paila")
+Calentar("huevos")
+Servir("jugo de manzanas")
